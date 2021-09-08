f97eaa0182c09bd78530234cf37fd5afd850e6be
Riya Jain <riyaj5246@gmail.com>
2021-09-07 19:22:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/test_app_1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.test_app_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.GestureDetector;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class MainActivity extends AppCompatActivity {
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    float x1, x2, y1, y2;
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 < x2){
+                    setContentView(R.layout.right_file);
+                }
+                else if(x1 > x2){
+                    setContentView(R.layout.left_screen);
+                }
+            break;
+        }
+        return false;
+    }
+
+    public void sendMessage(View view) {
+        CheckBox mainBox = (CheckBox) findViewById(R.id.checkBox1);
+        if(mainBox.isChecked()){
+            counter++;
+        }
+        changeText(counter + "");
+    }
+
+    private void changeText(String x){
+        TextView newtext2 = (TextView) findViewById(R.id.textview1);
+        newtext2.setText(x);
+    }
+}
---
+++ app/src/main/java/com/example/test_app_1/SwipeRight.java
@@ -0,0 +1,7 @@
+package com.example.test_app_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
